7f6677e7e3340ffced303b7ea0738ceb0a551d19
liuyaoduan <lyd1992827>
2018-06-25 17:32:58 +0800

    something added

---
+++ src/main/java/common/entity/Vehicle.java
@@ -0,0 +1,10 @@
+package common.entity;
+
+public class Vehicle {
+    public String vehicleId;
+    public String vehicleName;
+    public String vehicleNum;
+    public String lineId;
+    public String deviceId;
+
+}
